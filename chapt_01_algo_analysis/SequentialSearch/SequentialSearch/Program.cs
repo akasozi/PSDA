ff29a551827938c821c90e61a92f4d771b14fba5
Abu Kasozi <abukasozi@gmail.com>
2021-11-19 19:02:43 +0300

    First Commit

---
+++ chapt_01_algo_analysis/SequentialSearch/SequentialSearch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SequentialSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static int SequentialSearch(int[] arr, int size, int value) {
+
+            for (int i = 0; i < size; i++) {
+                if (arr[i] == value)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
---
+++ chapt_01_algo_analysis/SumArray/SumArray/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int total = SumArray(arr);
+            Console.Write($"Sum of values in array: ");
+            PrintArray(arr);
+            Console.Write($" is {total}");
+        }
+
+        static void PrintArray(int[] arr) {
+
+            int size = arr.Length;
+            if (size <= 0)
+                throw new Exception("Empty Array!");
+
+            Console.Write("{");
+            for (int i = 0; i < size - 1; i++)
+                Console.Write($"{arr[i]}, ");
+            Console.Write($"{arr[size - 1]}");
+            Console.Write("}");
+        }
+
+        static int SumArray(int[] arr) {
+
+            int size = arr.Length;
+            int total = 0;
+
+            for (int i = 0; i < size; i++) {
+                total += arr[i];
+            }
+            return total;
+        }
+    }
+}
